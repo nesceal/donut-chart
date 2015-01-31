1e82e5d0385b1f407ab0002ea9a0a806a856b91a
Nesceal <nesceal@gmail.com>
2015-01-31 13:45:38 -0800

    minor changes

--- js/main.js
+++ js/main.js
@@ -3,22 +3,22 @@ $( document ).ready(function() {
 	svg.append("g").attr("id","theDonut");
 	drawDonut();
 	for(var i=0, j=1; i<len; i++, j++){
-		$('table').append('<tr id="wedge_'+i+'"><td id="no_'+i+'">Wedge #'+j+'</td><td>Label:</td><td><input type="text" id="label_'+i+'" value="'+wedges[i].label+'" placeholder="'+wedges[i].label+'"></td><td>Percentage:</td><td><input type="text" id="percent_'+i+'" value="'+wedges[i].value+'" placeholder="'+wedges[i].value+'"></td><td>Color:</td><td><input type="text" id="color_'+i+'" value="'+wedges[i].color+'" placeholder="'+wedges[i].color+'"></td><td><button id="up_'+i+'" onclick="updateWedge('+i+')">Update</button></td><td><button id="rem_'+i+'" onclick="removeWedge('+i+')">Remove</button></td></tr>');
+		$('table').append('<tr id="wedge_'+i+'"><td id="no_'+i+'">Wedge #'+j+'</td><td>Label:</td><td><input type="text" id="label_'+i+'" value="'+wedges[i].label+'" placeholder="'+wedges[i].label+'"></td><td>Value:</td><td><input type="text" id="value_'+i+'" value="'+wedges[i].value+'" placeholder="'+wedges[i].value+'"></td><td>Color:</td><td><input type="text" id="color_'+i+'" value="'+wedges[i].color+'" placeholder="'+wedges[i].color+'"></td><td><button id="up_'+i+'" onclick="updateWedge('+i+')">Update</button></td><td><button id="rem_'+i+'" onclick="removeWedge('+i+')">Remove</button></td></tr>');
 	}
 	addWedge();
 });
 
 var wedges=[
-	{label:"Basic", value:20, color:"#DA9E90"},
-	{label:"Plus", value:20, color:"#86C44F"},
-	{label:"Deluxe", value:30, color:"#909ADA"},
-	{label:"Elite", value:30, color:"#A066CC"},
+	{label:"Basic", value:25, color:"#DA9E90"},
+	{label:"Plus", value:25, color:"#86C44F"},
+	{label:"Deluxe", value:35, color:"#909ADA"},
+	{label:"Elite", value:35, color:"#A066CC"},
 ];
 
 var len = wedges.length;
 
 function addWedge(){
-	$('table').append('<tr id="wedge_'+len+'"><td id="no_'+len+'">Wedge #'+(len+1)+'</td><td>Label:</td><td><input type="text" id="label_'+len+'" value="" placeholder=""></td><td>Percentage:</td><td><input type="text" id="percent_'+len+'" value="" placeholder=""></td><td>Color:</td><td><input type="text" id="color_'+len+'" value="" placeholder=""></td><td id="sub" colspan="2"><button id="sub_button" onclick="submitWedge('+len+')">Add Wedge</button></td></tr>');
+	$('table').append('<tr id="wedge_'+len+'"><td id="no_'+len+'">Wedge #'+(len+1)+'</td><td>Label:</td><td><input type="text" id="label_'+len+'" value="" placeholder=""></td><td>Value:</td><td><input type="text" id="value_'+len+'" value="" placeholder=""></td><td>Color:</td><td><input type="text" id="color_'+len+'" value="" placeholder=""></td><td id="sub" colspan="2"><button id="sub_button" onclick="submitWedge('+len+')">Add Wedge</button></td></tr>');
 		wedges.push({
 		label: '',
 		value: '',
@@ -35,16 +35,16 @@ function submitWedge(x){
 			'value': $('#label_'+x).val(),
 			'placeholder': $('#label_'+x).val()
 		});
-		$('#percent_'+x).attr({
-			'value': $('#percent_'+x).val(),
-			'placeholder': $('#percent_'+x).val()
+		$('#value_'+x).attr({
+			'value': $('#value_'+x).val(),
+			'placeholder': $('#value_'+x).val()
 		});
 		$('#color_'+x).attr({
 			'value': $('#color_'+x).val(),
 			'placeholder': $('#color_'+x).val()
 		});
 		wedges[x].label = $('#label_'+x).val();
-		wedges[x].value = $('#percent_'+x).val();
+		wedges[x].value = $('#value_'+x).val();
 		wedges[x].color = $('#color_'+x).val();
 		drawDonut();
 		$('span').text('You added a new wedge!');
@@ -59,16 +59,16 @@ function updateWedge(x){
 		'value': $('#label_'+x).val(),
 		'placeholder': $('#label_'+x).val()
 	});
-	$('#percent_'+x).attr({
-		'value': $('#percent_'+x).val(),
-		'placeholder': $('#percent_'+x).val()
+	$('#value_'+x).attr({
+		'value': $('#value_'+x).val(),
+		'placeholder': $('#value_'+x).val()
 	});
 	$('#color_'+x).attr({
 		'value': $('#color_'+x).val(),
 		'placeholder': $('#color_'+x).val()
 	});
 	wedges[x].label = $('#label_'+x).val();
-	wedges[x].value = $('#percent_'+x).val();
+	wedges[x].value = $('#value_'+x).val();
 	wedges[x].color = $('#color_'+x).val();
 	Donut3D.transition("theDonut", wedges, 130, 100, 20, 0.4);
 	$('span').text('You updated wedge #'+(x+1)+'!');
@@ -96,8 +96,8 @@ function removeWedge(x){
 			'value': wedges[i].label,
 			'placeholder': wedges[i].label
 		});
-		$('#percent_'+(i+1)).attr({
-			'id': 'percent_'+i,
+		$('#value_'+(i+1)).attr({
+			'id': 'value_'+i,
 			'value': wedges[i].value,
 			'placeholder': wedges[i].value
 		});
